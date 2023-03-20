cc6fdcc456770ad4087ed970fbd6aa2fd82016dd
anhcao286 <99060094+anhcao286@users.noreply.github.com>
2023-03-20 10:58:56 +0700

    Update get_coins.py

--- get_coins.py
+++ get_coins.py
@@ -1,20 +1,23 @@
 import xlwings as xw
 import requests
 import datetime
+from concurrent.futures import ThreadPoolExecutor
+
 # @xw.sub
 def extract_data():
     # Define the URLs for the APIs
-    # price_url = "https://fapi.binance.com/fapi/v1/ticker/price"
     price_url = "https://fapi.binance.com/fapi/v1/premiumIndex"
     last_price_url = "https://fapi.binance.com/fapi/v1/ticker/24hr"
     
-    # Send requests to the APIs and get the responses
-    price_response = requests.get(price_url)
-    last_price_response = requests.get(last_price_url)
-    
-    # Convert the responses to JSON format
-    price_data = price_response.json()
-    last_price_data = last_price_response.json()
+    # Create a ThreadPoolExecutor to make the API requests in parallel
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        # Send requests to the APIs and get the responses in parallel
+        price_response = executor.submit(requests.get, price_url)
+        last_price_response = executor.submit(requests.get, last_price_url)
+        
+        # Convert the responses to JSON format
+        price_data = price_response.result().json()
+        last_price_data = last_price_response.result().json()
     
     # Create a list to store the data
     data = []
@@ -42,4 +45,6 @@ def extract_data():
     sheet.clear_contents()
     sheet.range("A1").value = ["Symbol", "Mark Price", "Funding Rate", "Last Price","Time"]
     sheet.range("A2").value = data
+    # Autofit the columns
+    sheet.range("A1").expand("table").columns.autofit()
 
