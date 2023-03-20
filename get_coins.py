c78cbace37336677dd1d0ff96f4138506cf7eb19
anhcao286 <99060094+anhcao286@users.noreply.github.com>
2023-03-20 10:49:24 +0700

    Update get_coins.py

--- get_coins.py
+++ get_coins.py
@@ -1,38 +1,30 @@
 import xlwings as xw
 import requests
-
+import datetime
 # @xw.sub
 def extract_data():
     # Define the URLs for the APIs
-    price_url = "https://fapi.binance.com/fapi/v1/ticker/price"
-    funding_rate_url = "https://fapi.binance.com/fapi/v1/fundingRate"
+    # price_url = "https://fapi.binance.com/fapi/v1/ticker/price"
+    price_url = "https://fapi.binance.com/fapi/v1/premiumIndex"
     last_price_url = "https://fapi.binance.com/fapi/v1/ticker/24hr"
     
     # Send requests to the APIs and get the responses
     price_response = requests.get(price_url)
-    funding_rate_response = requests.get(funding_rate_url)
     last_price_response = requests.get(last_price_url)
     
     # Convert the responses to JSON format
     price_data = price_response.json()
-    funding_rate_data = funding_rate_response.json()
     last_price_data = last_price_response.json()
     
     # Create a list to store the data
     data = []
-    
     # Loop through the price data and extract the symbol and price
     for item in price_data:
         symbol = item["symbol"]
-        price = item["price"]
-        
-        # Loop through the funding rate data and find the corresponding funding rate
-        for rate in funding_rate_data:
-            if rate["symbol"] == symbol:
-                funding_rate = rate["fundingRate"]
-                break
-        else:
-            funding_rate = "N/A"
+        price = item["markPrice"]
+        funding_rate = item['lastFundingRate']
+        timestamp = item['time']
+        time = datetime.datetime.fromtimestamp(timestamp / 1000).strftime('%H:%M %d/%m/%Y')
         
         # Loop through the last price data and find the corresponding last price
         for last_price in last_price_data:
@@ -43,10 +35,11 @@ def extract_data():
             last_price = "N/A"
         
         # Append the data to the list
-        data.append([symbol, price, funding_rate, last_price])
+        data.append([symbol, price, funding_rate, last_price,time])
     
     # Write the data to the Excel sheet
     sheet = xw.sheets.active
     sheet.clear_contents()
-    sheet.range("A1").value = ["Symbol", "Price", "Funding Rate", "Last Price"]
+    sheet.range("A1").value = ["Symbol", "Mark Price", "Funding Rate", "Last Price","Time"]
     sheet.range("A2").value = data
+
